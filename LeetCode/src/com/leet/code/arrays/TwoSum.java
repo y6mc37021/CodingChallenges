9c3ebcc21a4c73cfd9c905e1bf86909c0fd77f09
y6mc37021 <tkarthik21@gmail.com>
2023-05-16 21:51:35 +0100

    Arrays

---
+++ LeetCode/src/com/leet/code/arrays/TwoSum.java
@@ -0,0 +1,55 @@
+package com.leet.code.arrays;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+//https://leetcode.com/problems/two-sum/ 
+//1. Two Sum
+public class TwoSum {
+
+	public static void main(String[] args) {
+		int[] arr = {3,7,4,1,2,};
+		int target = 9;
+		solution(arr,target);
+		//betterSolution(arr,target);
+	}
+	
+	public static void solution_1(int[] arr, int target ) {
+		
+		int[] tarr = new int[2];
+		for(int i=0;i<arr.length;i++) {
+			for(int j=i+1;j<arr.length;j++) {
+				if((arr[i]+arr[j])==target) {
+					tarr[0]=i;
+					tarr[1]=j;
+				}
+				
+			}
+		}
+		
+		System.out.println("Output Length = "+tarr.length);
+		System.out.println("Output = "+Arrays.toString(tarr));
+		
+	}
+	
+	public static int[] solution(int[] nums, int target) {
+	        int n=nums.length;
+	        Map<Integer,Integer> map=new HashMap<>();
+	        int[] result=new int[2];
+	        for(int i=0;i<n;i++){
+	            if(map.containsKey(target-nums[i])){
+	                result[1]=i;
+	                result[0]=map.get(target-nums[i]);
+	                return result;
+	            }
+	            map.put(nums[i],i);
+	        }
+	        
+	        return result;
+
+	}
+	
+	
+
+}
---
+++ LeetCode/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author NAVYA
+ *
+ */
+module Leetcode {
+}
